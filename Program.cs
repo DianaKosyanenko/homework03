3089548e2c76ee0e5362847a52e99a5be55a74b2
Diana <kosyanenkodiana@yandex.ru>
2022-12-18 18:13:33 +0300

    rep

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/*void Test(int num){
+  int thous = num / 10000;
+  int ed = num % 10;
+  if (num < 10000 || num > 99999)
+  {
+    Console.WriteLine("Error");
+  }
+  else if (thous == ed)
+  Console.WriteLine("It is palendrom");
+  else
+  Console.WriteLine("It is not palendrom");
+}
+Console.WriteLine("Enter your number");
+int user = Convert.ToInt32(Console.ReadLine());
+Test(user);*/
+
+/*double Geo(int x1, int x2, int y1, int y2, int z1, int z2){
+    double d =(x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1);
+    double g = Math.Sqrt(d);
+    return(g);
+}
+Console.WriteLine("Enter x1");
+int x1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter x2");
+int x2 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter y1");
+int y1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter y2");
+int y2 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter z1");
+int z1 =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter z2");
+int z2 =Convert.ToInt32(Console.ReadLine());
+double res = Geo(x1, x2, y1, y2, z1, z2);
+Console.WriteLine($"result-{Math.Round(res,2)}");*/
+
+/*void Table(int N){
+    for (int number1 = 1; number1 <= N; number1++)
+    {
+        Console.Write(number1*number1*number1 + " ");
+    }
+}
+Console.WriteLine("Entre your number:");
+int user = Convert.ToInt32(Console.ReadLine());
+Table(user);*/
